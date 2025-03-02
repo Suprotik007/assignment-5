2fb454cf1580f96432c7e540c8c7edf82958a0d3
Suprotik007 <csuprotik@gmail.com>
2025-03-02 21:08:51 +0600

    completed

--- assets/board.js
+++ assets/board.js
@@ -4,10 +4,12 @@ document.getElementById('discover').addEventListener('click',function(event){
      window.location.href="./ques.html"
 })
 
+
 // theme
 document.getElementById('theme-btn').addEventListener('click',function(event){
     event.preventDefault()
     let colors = ["#FF5733", "#33FF57", "#3357FF", "#F3FF33", "#FF33A1"]
+    
     let color = colors[Math.floor(Math.random() * colors.length)]
     document.body.style.backgroundColor = color
 })
@@ -15,7 +17,7 @@ document.getElementById('theme-btn').addEventListener('click',function(event){
 
 // time
 let realDate = new Date().toDateString()
-document.getElementById("realdate").innerText = realDate 
+document.getElementById("real-date").innerText = realDate 
 
 // clear
 
@@ -35,23 +37,32 @@ for (let x = 0; x < Button.length; x++) {
         
     })
 }
-
+// task-functions
 
 document.addEventListener("DOMContentLoaded", function () {
     const eachButton = document.querySelectorAll(".com-btn")
     const task1 = document.getElementById("task-1")
     const task2 = document.getElementById("task-2")
+    const logAct =document.getElementById('act-log')
     let totalClick = 0;
 
 
     eachButton.forEach((button) => {
         button.addEventListener("click", function () {
             
-            let currentTask1 = parseInt(task1.innerText)
-            let currentTask2 = parseInt(task2.innerText)
+            let Task1 = parseInt(task1.innerText)
+            let Task2 = parseInt(task2.innerText)
 
-            task1.innerText = currentTask1 + 1
-            task2.innerText =  currentTask2 - 1 
+            task1.innerText = Task1 + 1
+            task2.innerText =  Task2 - 1 
+
+            const taskTitle = this.closest(".card").querySelector(".card-title").innerText;
+            const logEntry = document.createElement("p");
+            
+            logEntry.innerHTML = `You have done the task <strong style="color: green;">"${taskTitle}"</strong> at <strong>${new Date().toLocaleTimeString()}</strong>`;
+ 
+            logAct.prepend(logEntry)
+
 
             totalClick++;
 
@@ -59,10 +70,10 @@ document.addEventListener("DOMContentLoaded", function () {
             if (totalClick === 6) {
                 alert("'Board updated successfully'")
                 alert("Congratulations! You've completed all tasks!")
-            } else {
-                alert("Board updated successfully")
-            }
+            } 
         })
         
     })
 })
+
+ 
